0729890c634b55b0b6891d3b63cd5dddac068e77
Mari Vos <marivos50@yahoo.es>
2021-12-11 18:14:39 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -8,7 +8,7 @@ from PIL import Image
 
 st.markdown("# Self Exploratory Visualization on palmerpenguins")
 st.markdown("Explore the dataset to know more about palmerpenguins")
-img=Image.open("logo.PNG")
+img=Image.open("logo.png")
 st.image(img,width=100)
 st.markdown("**Penguins** are some of the most recognizable and beloved birds in the world and even have their own holiday: **World Penguin Day is celebrated every year on April 25**. Penguins are also amazing birds because of their physical adaptations to survive in unusual climates and to live mostly at sea. Penguins propel themselves through water by flapping their flippers. Bills tend to be long and thin in species that are primarily fish eaters, and shorter and stouter in those that mainly eat krill.")
 st.markdown("The data presented are of 3 different species of penguins - **Adelie, Chinstrap, and Gentoo,** collected from 3 islands in the **Palmer Archipelago, Antarctica.**")
