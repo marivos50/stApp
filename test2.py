b84d6eaf32caeccaa189df8394b59afcd39b7a28
Mari Vos <marivos50@yahoo.es>
2021-12-12 14:23:13 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -6,11 +6,11 @@ from PIL import Image
 #import numpy as np
 
 
-st.markdown("# stApp")
+st.markdown("# MariChat")
 st.markdown("Explore the dataset to know more about palmerpenguins")
 img=Image.open("yo.jpeg")
 st.image(img,width=100)
-st.markdown("**Marichat** Un chatbot para preguntar sobre programación in Python")
+st.markdown("* Un chatbot para preguntar sobre programación in Python*")
 st.text_area("usuario", value="", height=None, max_chars=None, key=None, help="escribe tu pregunta aquí")
 if st.button("Saludame"):    
     st.markdown("Me llamo Mari, y te doy un beso de bienvenida a mi pagina streamlit")
