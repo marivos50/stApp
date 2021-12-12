c0758ae09c5564f5c097d5a5b100e4bc73f60577
Mari Vos <marivos50@yahoo.es>
2021-12-12 13:49:11 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -6,16 +6,13 @@ from PIL import Image
 #import numpy as np
 
 
-st.markdown("# Self Exploratory Visualization on palmerpenguins")
+st.markdown("# stApp")
 st.markdown("Explore the dataset to know more about palmerpenguins")
-img=Image.open("logo.png")
+img=Image.open("yo.png")
 st.image(img,width=100)
-st.markdown("**Penguins** are some of the most recognizable and beloved birds in the world and even have their own holiday: **World Penguin Day is celebrated every year on April 25**. Penguins are also amazing birds because of their physical adaptations to survive in unusual climates and to live mostly at sea. Penguins propel themselves through water by flapping their flippers. Bills tend to be long and thin in species that are primarily fish eaters, and shorter and stouter in those that mainly eat krill.")
-st.markdown("The data presented are of 3 different species of penguins - **Adelie, Chinstrap, and Gentoo,** collected from 3 islands in the **Palmer Archipelago, Antarctica.**")
-
-if st.button("Saludame"):
-    img=Image.open("yo.jpeg")
-    st.image(img,width=700, caption="Te saludo")
+st.markdown("**Marichat** Un chatbot para preguntar sobre programación in Python")
+st.text_area(label="usuario", value="", height=None, max_chars=None, key=None, help="escribe tu pregunta aquí", on_change=None, args=None, kwargs=None, *, placeholder=None)
+if st.button("Saludame"):    
     st.markdown("Me llamo Mari, y te doy un beso de bienvenida a mi pagina streamlit")
     #Ballons
     st.balloons()
